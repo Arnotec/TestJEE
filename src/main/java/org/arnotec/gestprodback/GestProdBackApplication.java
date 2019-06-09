3bf76e5cd0d38d7c79b9898fc62fb3e919738e72
Pierre Arnaud <p.tsofack.megniguim@accenture.com>
2019-06-09 13:04:24 +0400

    Start project

---
+++ src/main/java/org/arnotec/gestprodback/GestProdBackApplication.java
@@ -0,0 +1,14 @@
+package org.arnotec.gestprodback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestProdBackApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GestProdBackApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/arnotec/gestprodback/entities/Produit.java
@@ -0,0 +1,57 @@
+package org.arnotec.gestprodback.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Produit implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 60)
+    private String designation;
+    private double prix;
+    private int quantite;
+
+    public Produit() {
+    }
+
+    public Produit(Long id, String designation, double prix, int quantite) {
+        this.id = id;
+        this.designation = designation;
+        this.prix = prix;
+        this.quantite = quantite;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
+}
